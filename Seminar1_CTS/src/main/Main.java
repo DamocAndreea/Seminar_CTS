3ad44309ef73ba178cd81bee676b159dbbc439ca
DamocAndreea <damoc.andreea@yahoo.com>
2021-03-03 23:07:36 +0200

    Girafe&zebre

--- Seminar1_CTS/src/main/Main.java
+++ Seminar1_CTS/src/main/Main.java
@@ -1,10 +1,33 @@
 package main;
 
+import classes.Giraffe;
+import classes.Zebra;
+import classes.Zoo;
+import classes.Zookeeper;
+
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, world!");
-
+		Zookeeper keeper = new Zookeeper("Costel");
+		Zoo zoo=new Zoo();
+		
+		zoo.setZookeeper(keeper);
+		
+		Giraffe giraffe = new Giraffe("Girafa", 10);
+		Giraffe secondGiraffe = new Giraffe("GirafaDoi", 15, 200);
+		
+		zoo.addAnimal(giraffe);
+		zoo.addAnimal(secondGiraffe);
+		
+		zoo.feedAllAnimals();
+		
+		Zebra z1= new Zebra("Zebra1", 7);
+		Zebra z2= new Zebra("Zebra2", 9, 130);
+		
+		zoo.addAnimal(z1);
+		zoo.addAnimal(z2);
+		
+		zoo.feedAllAnimals();
 	}
 
 }
