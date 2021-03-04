60d4b6549e7adb169457d6cb8f3e8d528dcae772
DamocAndreea <damoc.andreea@yahoo.com>
2021-03-04 08:25:50 +0200

    Readers+fisiere

--- Seminar2_CTS/src/ro/ase/cts/main/Program.java
+++ Seminar2_CTS/src/ro/ase/cts/main/Program.java
@@ -1,6 +1,7 @@
 package ro.ase.cts.main;
 
-import ro.ase.cts.classes.Utils;
+import ro.ase.cts.readers.AngajatReader;
+
 import java.io.FileNotFoundException;
 import java.util.List;
 
@@ -11,7 +12,7 @@ public class Program {
 	public static void main(String[] args) {
 		List<Angajat> listaAngajati;
 		try {
-			listaAngajati = Utils.readAngajati("angajati.txt");
+			listaAngajati = AngajatReader.readAngajati("angajati.txt");
 			for(Angajat angajat:listaAngajati)
 				System.out.println(angajat.toString());
 		} catch (FileNotFoundException e) {
---
+++ Seminar2_CTS/src/ro/ase/cts/readers/AplicantReader.java
@@ -0,0 +1,11 @@
+package ro.ase.cts.readers;
+
+public class AplicantReader {
+	private String fileName;
+
+	public String getFileName() {
+		return fileName;
+	}
+	
+	
+}
