e9debcd2f526a9d43dd581c6b02bd6583e6b049a
DamocAndreea <damoc.andreea@yahoo.com>
2021-03-04 09:23:02 +0200

    Modificari readers

--- Seminar2_CTS/src/ro/ase/cts/main/Program.java
+++ Seminar2_CTS/src/ro/ase/cts/main/Program.java
@@ -1,19 +1,27 @@
 package ro.ase.cts.main;
 
 import ro.ase.cts.readers.AngajatReader;
+import ro.ase.cts.readers.AplicantReader;
 
 import java.io.FileNotFoundException;
 import java.util.List;
 
 import ro.ase.cts.classes.Angajat;
+import ro.ase.cts.classes.Aplicant;
 
 public class Program {
 
+	public static List<Aplicant> citesteAplicanti(AplicantReader reader) throws NumberFormatException, FileNotFoundException{
+		return reader.citesteAplicanti();
+	}
+	
 	public static void main(String[] args) {
-		List<Angajat> listaAngajati;
+		
+		List<Aplicant> listaAplicanti;
+		
 		try {
-			listaAngajati = AngajatReader.readAngajati("angajati.txt");
-			for(Angajat angajat:listaAngajati)
+			listaAplicanti = citesteAplicanti(new AngajatReader("angajati.txt"));
+			for(Aplicant angajat:listaAplicanti)
 				System.out.println(angajat.toString());
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
--- Seminar2_CTS/src/ro/ase/cts/readers/AplicantReader.java
+++ Seminar2_CTS/src/ro/ase/cts/readers/AplicantReader.java
@@ -1,7 +1,19 @@
 package ro.ase.cts.readers;
+import java.io.FileNotFoundException;
+import java.util.List;
 
-public class AplicantReader {
+import ro.ase.cts.classes.Aplicant;
+
+public abstract class AplicantReader {
+	
 	private String fileName;
+	
+	public abstract List<Aplicant> citesteAplicanti() throws FileNotFoundException, NumberFormatException;
+
+	public AplicantReader(String fileName) {
+		super();
+		this.fileName = fileName;
+	}
 
 	public String getFileName() {
 		return fileName;
