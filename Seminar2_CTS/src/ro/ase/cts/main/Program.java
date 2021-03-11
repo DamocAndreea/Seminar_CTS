f120b6fc39e7fd7ba2f72aba0f47d54b1d3e37b9
DamocAndreea <damoc.andreea@yahoo.com>
2021-03-11 08:35:33 +0200

    Modificari clase

--- Seminar2_CTS/src/ro/ase/cts/main/Program.java
+++ Seminar2_CTS/src/ro/ase/cts/main/Program.java
@@ -2,26 +2,27 @@ package ro.ase.cts.main;
 
 import ro.ase.cts.readers.AngajatReader;
 import ro.ase.cts.readers.AplicantReader;
+import ro.ase.cts.readers.StudentReader;
 
 import java.io.FileNotFoundException;
 import java.util.List;
 
-import ro.ase.cts.classes.Angajat;
 import ro.ase.cts.classes.Aplicant;
 
 public class Program {
 
-	public static List<Aplicant> citesteAplicanti(AplicantReader reader) throws NumberFormatException, FileNotFoundException{
+	public static List<Aplicant> citesteAplicanti(AplicantReader reader)
+			throws NumberFormatException, FileNotFoundException {
 		return reader.citesteAplicanti();
 	}
-	
+
 	public static void main(String[] args) {
-		
+
 		List<Aplicant> listaAplicanti;
-		
+		AplicantReader aplicantReader = new StudentReader("studenti.txt");
 		try {
 			listaAplicanti = citesteAplicanti(new AngajatReader("angajati.txt"));
-			for(Aplicant angajat:listaAplicanti)
+			for (Aplicant angajat : listaAplicanti)
 				System.out.println(angajat.toString());
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
--- Seminar2_CTS/src/ro/ase/cts/readers/StudentReader.java
+++ Seminar2_CTS/src/ro/ase/cts/readers/StudentReader.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-import ro.ase.cts.classes.Angajat;
 import ro.ase.cts.classes.Aplicant;
 import ro.ase.cts.classes.Student;
 
@@ -22,21 +21,16 @@ public class StudentReader extends AplicantReader {
 		List<Aplicant> aplicants = new ArrayList<Aplicant>();
 
 		while (input.hasNext()) {
-			String nume = input.next();
-			String prenume = (input.next()).toString();
-			int varsta = Integer.valueOf(input.nextInt());
-			int punctaj = Integer.valueOf(input.nextInt());
-			int nr = Integer.valueOf(input.nextInt());
-			String[] vect = new String[5];
-			for (int i = 0; i < nr; i++)
-				vect[i] = input.next();
+			Student student = new Student();
+			super.citesteAplicant(input, student);
 			int an_studii = input.nextInt();
 			String facultate = (input.next()).toString();
-			Student s = new Student(nume, prenume, varsta, punctaj, nr, vect, facultate, an_studii);
-			aplicants.add(s);
+			student.setAn_studii(an_studii);
+			student.setFacultate(facultate);
+			aplicants.add(student);
 		}
 		input.close();
 		return aplicants;
 	}
-	
+
 }
