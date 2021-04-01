149219cdf987761b5ca95ba18256299d7ee0cad7
DamocAndreea <damoc.andreea@yahoo.com>
2021-04-01 09:48:25 +0300

    BuilderV3

--- Seminar6_CTS/src/ro/ase/cts/builderV3/classes/IBuilderV3.java
+++ Seminar6_CTS/src/ro/ase/cts/builderV3/classes/IBuilderV3.java
@@ -1,6 +1,5 @@
 package ro.ase.cts.builderV3.classes;
 
-
 public interface IBuilderV3 {
 	RezervareV3 build();
 }
--- Seminar6_CTS/src/ro/ase/cts/builderV3/classes/RezervareV3.java
+++ Seminar6_CTS/src/ro/ase/cts/builderV3/classes/RezervareV3.java
@@ -7,13 +7,14 @@ public class RezervareV3 {
 	private boolean muzicaAmbientala;
 	private String genMuzica;
 	private int codRezervare;
-	
+
 	@Override
 	public String toString() {
 		return "Rezervare [mancareinclusa=" + mancareinclusa + ", scaunErgonomic=" + scaunErgonomic
 				+ ", bauturaRacoritoare=" + bauturaRacoritoare + ", muzicaAmbientala=" + muzicaAmbientala
 				+ ", genMuzica=" + genMuzica + ", codRezervare=" + codRezervare + "]";
 	}
+
 	public RezervareV3(Builder builder) {
 		super();
 		this.mancareinclusa = builder.mancareinclusa;
@@ -23,81 +24,92 @@ public class RezervareV3 {
 		this.genMuzica = builder.genMuzica;
 		this.codRezervare = builder.codRezervare;
 	}
+
 	public boolean isMancareinclusa() {
 		return mancareinclusa;
 	}
+
 	public void setMancareinclusa(boolean mancareinclusa) {
 		this.mancareinclusa = mancareinclusa;
 	}
+
 	public boolean isScaunErgonomic() {
 		return scaunErgonomic;
 	}
+
 	public void setScaunErgonomic(boolean scaunErgonomic) {
 		this.scaunErgonomic = scaunErgonomic;
 	}
+
 	public boolean isBauturaRacoritoare() {
 		return bauturaRacoritoare;
 	}
+
 	public void setBauturaRacoritoare(boolean bauturaRacoritoare) {
 		this.bauturaRacoritoare = bauturaRacoritoare;
 	}
+
 	public boolean isMuzicaAmbientala() {
 		return muzicaAmbientala;
 	}
+
 	public void setMuzicaAmbientala(boolean muzicaAmbientala) {
 		this.muzicaAmbientala = muzicaAmbientala;
 	}
+
 	public String getGenMuzica() {
 		return genMuzica;
 	}
+
 	public void setGenMuzica(String genMuzica) {
 		this.genMuzica = genMuzica;
 	}
+
 	public int getCodRezervare() {
 		return codRezervare;
 	}
+
 	public void setCodRezervare(int codRezervare) {
 		this.codRezervare = codRezervare;
 	}
-	
-	public static Builder builder()
-	{
+
+	public static Builder builder() {
 		return new Builder();
 	}
-	public static class Builder implements IBuilderV3
-	{
+
+	public static class Builder implements IBuilderV3 {
 		private boolean mancareinclusa;
 		private boolean scaunErgonomic;
 		private boolean bauturaRacoritoare;
 		private boolean muzicaAmbientala;
 		private String genMuzica;
 		private int codRezervare;
-		
+
 		public Builder setMancareinclusa(boolean mancareinclusa) {
 			this.mancareinclusa = mancareinclusa;
 			return this;
 		}
-		
+
 		public Builder setScaunErgonomic(boolean scaunErgonomic) {
 			this.scaunErgonomic = scaunErgonomic;
 			return this;
-			}
-		
+		}
+
 		public Builder setBauturaRacoritoare(boolean bauturaRacoritoare) {
 			this.bauturaRacoritoare = bauturaRacoritoare;
 			return this;
 		}
-		
+
 		public Builder setMuzicaAmbientala(boolean muzicaAmbientala) {
 			this.muzicaAmbientala = muzicaAmbientala;
 			return this;
 		}
-		
+
 		public Builder setGenMuzica(String genMuzica) {
 			this.genMuzica = genMuzica;
 			return this;
 		}
-		
+
 		public Builder setCodRezervare(int codRezervare) {
 			this.codRezervare = codRezervare;
 			return this;
