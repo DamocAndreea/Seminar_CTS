c4a6105946cd0417be5558828c4e49bd1d317933
DamocAndreea <damoc.andreea@yahoo.com>
2021-04-15 08:53:29 +0300

    Proxy

---
+++ Seminar8_CTS/src/ro/ase/cts/proxy/IRezervare.java
@@ -0,0 +1,5 @@
+package ro.ase.cts.proxy;
+
+public interface IRezervare {
+	void anuntaRezervare();
+}
---
+++ Seminar8_CTS/src/ro/ase/cts/proxy/main/Program.java
@@ -0,0 +1,20 @@
+package ro.ase.cts.proxy.main;
+
+import ro.ase.cts.proxy.IRezervare;
+import ro.ase.cts.proxy.ProxyRezervare;
+import ro.ase.cts.proxy.Rezervare;
+
+public class Program {
+
+	public static void main(String[] args) {
+		IRezervare rezervarePentruDouaPersoane = new Rezervare(3,2);
+		rezervarePentruDouaPersoane.anuntaRezervare();
+		
+		ProxyRezervare proxy = new ProxyRezervare((Rezervare)rezervarePentruDouaPersoane);
+		proxy.anuntaRezervare();
+		
+		((Rezervare)rezervarePentruDouaPersoane).setNrPersoane(4);
+		proxy.anuntaRezervare();
+	}
+
+}
