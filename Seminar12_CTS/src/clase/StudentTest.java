d2876f6aa857fe5f769acebd7c885177670e272a
DamocAndreea <damoc.andreea@yahoo.com>
2021-05-20 08:19:31 +0300

    teste

---
+++ Seminar12_CTS/src/clase/StudentTest.java
@@ -0,0 +1,41 @@
+package clase;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class StudentTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testConstructorRightName() {
+		String nume = "Ion";
+		Student student = new Student(nume);
+		assertEquals(nume, student.getNume());
+	}
+
+	@Test
+	public void testListAddNota() {
+		Student student = new Student();
+		int nota = 8;
+		student.adaugaNota(nota);
+		assertEquals(nota, student.getNota(0));
+	}
+
+	@Test
+	public void testListSize() {
+		Student student = new Student();
+		int nota = 8;
+		student.adaugaNota(nota);
+		assertEquals(1, student.getNote().size());
+	}
+}
